ef1e2525655e33a3ca2600a9eba9546028107345
Goulstem <sk84life4evr@Gmail.com>
2026-05-26 18:24:12 -0700

    5/26/26 updates

--- game.js
+++ game.js
@@ -1,10 +1,9 @@
 //[[Declarations / variables]]
+import {clickEvent} from "./helperFunctions/clickCode.js";
 const game = document.getElementById("game");                           //`game` is the document element which we will load the game onto!
 let cards = ["🍎", "🍎", "🍌", "🍌", "🦎", "🦎", "🍇", "🍇",        //`cards` is all of the different types of cards that can show up!
     "🐨", "🐨", "🐔", "🐔", "☘️", "☘️", "🍀", "🍀"
 ]                               
-let needToMatch = null;         //`needToMatch` is responsible for logic of tracking matches (are there 0? are there1? are there 2?)
-let matches = [];               //`matches` list is to actually keep track of matches as we click them!
 
 //===============================================================
 //[[Helper functions]]
@@ -23,27 +22,7 @@ function createBoard() {
 
         /////////////////////////////////////////////
         //[[Clicking mechanics]]
-        card.addEventListener("click",(e)=>{
-            //When clicked, display the card's val!
-            let clickedElement = e.currentTarget;
-            let cardValue = clickedElement.dataset.value;
-            clickedElement.innerText = cardValue;
-            if (needToMatch == null) {              //First match click
-                needToMatch = cardValue
-                matches.push(clickedElement);
-            } else {
-                 if (needToMatch == cardValue){
-                    matches.push(clickedElement);
-                    console.log("Matched!");
-                    needToMatch = null;
-                    matches[0].classList.add("matched");
-                    matches[1].classList.add("matched");
-                 } else {
-                    matches = [];
-                    needToMatch = null;
-                 }
-            }
-        });
+        card.addEventListener("click",clickEvent);
         ////////////////////////////////////////////
         
         game.appendChild(card);
