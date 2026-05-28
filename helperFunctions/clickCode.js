348260f9238ad82ac3b3d25189d6f3f7065f7724
Josh Morvant <sk84life4evr@Gmail.com>
2026-05-27 18:00:26 -0700

    5/27/26

--- helperFunctions/clickCode.js
+++ helperFunctions/clickCode.js
@@ -1,29 +1,25 @@
-let needToMatch = null;         //`needToMatch` is responsible for logic of tracking matches (are there 0? are there1? are there 2?)
+let selectedCard = null;         //`selectedCard` is responsible for logic of tracking matches (are there 0? are there1? are there 2?)
 let matches = [];               //`matches` list is to actually keep track of matches as we click them!
 
 export function clickEvent(e){
     //When clicked, display the card's val!
     let clickedElement = e.currentTarget;
-    let cardValue = clickedElement.dataset.value;
-    clickedElement.innerText = cardValue;
-    if (needToMatch == null) {              //First match click
-        needToMatch = cardValue
+    let clickedCardValue = clickedElement.dataset.value;
+    clickedElement.innerText = clickedCardValue;
+    if (selectedCard == null) {                     //First match click
+        selectedCard = clickedCardValue
         matches.push(clickedElement);
         clickedElement.classList.add("selected");
-    } else {
-            if (needToMatch == cardValue){     //Pair check
-                matches.push(clickedElement);
-                needToMatch = null;
-                matches[0].classList.add("matched");
-                matches[1].classList.add("matched");
-                matches=[];
-            } else {                           //Swap check (clicked one, then another that did not match: switch to the latest clicked!)
-            if (!matches[1]){
-                matches[0].classList.remove("selected");
-                matches[0] = clickedElement;
-                matches[0].classList.add("selected");
-                needToMatch = cardValue;
-            }
-        }
+    } else if (selectedCard == clickedCardValue){   //Pair check
+        matches.push(clickedElement);
+        selectedCard = null;
+        matches[0].classList.add("matched");
+        matches[1].classList.add("matched");
+        matches=[];
+    } else {                                        //Swap check (clicked one, then another that did not match: switch to the latest clicked!)
+        matches[0].classList.remove("selected");
+        matches[0] = clickedElement;
+        matches[0].classList.add("selected");
+        selectedCard = clickedCardValue;
     }
 }
